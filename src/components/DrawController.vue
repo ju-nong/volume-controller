b5c7c5ac92ef759a615f794513270f354748845d
이준용 <iiiuooyoo@gmail.com>
2024-01-19 15:46:56 +0900

    feat : [그리기 컨트롤러] 그리기 추가

--- src/components/DrawController.vue
+++ src/components/DrawController.vue
@@ -1,15 +1,50 @@
 <template>
     <div class="container">
         <span>Volume</span>
-        <canvas></canvas>
+        <canvas
+            width="140"
+            height="10"
+            ref="$canavas"
+            @mousedown="handleMouseDown"
+            @mousemove="handleMouseMove"
+        ></canvas>
         <span>{{ volume }}%</span>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
 const volume = ref(0);
+
+const $canavas = ref<null | HTMLCanvasElement>(null);
+const $ctx = computed<null | CanvasRenderingContext2D>(() =>
+    $canavas.value === null ? null : $canavas.value.getContext("2d"),
+);
+
+const isMouseDown = ref(false);
+
+function handleMouseMove(event: MouseEvent) {
+    if (!isMouseDown.value || $ctx.value === null) {
+        return;
+    }
+
+    const { offsetX, offsetY } = event;
+    $ctx.value.fillStyle = "black";
+    $ctx.value.fillRect(offsetX, offsetY, 1, 1);
+}
+
+function handleMouseUp() {
+    isMouseDown.value = false;
+
+    document.removeEventListener("mouseup", handleMouseUp);
+}
+
+function handleMouseDown() {
+    isMouseDown.value = true;
+
+    document.addEventListener("mouseup", handleMouseUp);
+}
 </script>
 
 <style scoped>
@@ -24,8 +59,6 @@ const volume = ref(0);
     margin: 0 auto;
 
     > canvas {
-        width: 140px;
-        height: 10px;
         border: 1px solid #ccc;
     }
 }
